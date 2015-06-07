739835eb3169ccd65b63303c686090fc1bdef487
Markus Mayer <widemeadows@gmail.com>
2015-06-07 15:35:09 +0200

    working on REGD

--- Optimization/GradientDescent/ResilientErrorGradientDescent.cs
+++ Optimization/GradientDescent/ResilientErrorGradientDescent.cs
@@ -6,14 +6,39 @@ namespace widemeadows.Optimization.GradientDescent
     /// <summary>
     /// Resilient Error Gradient Descent.
     /// </summary>
-    public class ResilientErrorGradientDescent : IMinimization<double, ICostGradient<double>> 
+    public class ResilientErrorGradientDescent : IMinimization<double, ICostGradient<double>>
     {
+        /// <summary>
+        /// The maximum number of iterations
+        /// </summary>
+        private readonly int _maxIterations = 400;
+
         /// <summary>
         /// Minimizes the specified problem.
         /// </summary>
         /// <param name="problem">The problem.</param>
         public IOptimizationResult<double> Minimize(IOptimizationProblem<double, ICostGradient<double>> problem)
         {
+            var maxIterations = _maxIterations;
+            
+            // obtain the initial cost
+            var costFunction = problem.CostFunction;
+            var previousCost = double.MaxValue;
+
+            // obtain the initial coefficients
+            var hypothesis = problem.Hypothesis;
+            var coefficients = hypothesis.GetInitialCoefficients();
+
+            // loop over all allowed iterations
+            for (var i = 0; i < maxIterations; ++i)
+            {
+                // obtain the cost and its gradient
+                var costResult = costFunction.CalculateCost(coefficients);
+
+                // determine the change in cost
+                var costChange = costResult.Cost - previousCost;
+            }
+
             throw new NotImplementedException("resilient error gradient descent not implemented");
         }
     }
