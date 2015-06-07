b8bf865e1405c1910719e310373ce48c03c075ee
Markus Mayer <widemeadows@gmail.com>
2015-06-07 14:54:47 +0200

    added the test for the resilient error gradient descent

--- OptimizationTests/ResilientErrorGradientDescentTests.cs
+++ OptimizationTests/ResilientErrorGradientDescentTests.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
+using widemeadows.Optimization.Cost;
 using widemeadows.Optimization.Cost.SSE;
+using widemeadows.Optimization.GradientDescent;
+using widemeadows.Optimization.Hypotheses;
 
 namespace widemeadows.Optimization.Tests
 {
@@ -14,11 +13,30 @@ namespace widemeadows.Optimization.Tests
         [Test]
         public void ResidualSumOfSquares()
         {
-            /*
-            var hypothesis
+            // obtain the test data
+            var trainingSet = new List<DataPoint<double>>
+            {
+                new DataPoint<double>(-1, -2),
+                new DataPoint<double>(0, 0),
+                new DataPoint<double>(1, 2),
+                new DataPoint<double>(2, 4),
+                new DataPoint<double>(3, 6)
+            };
 
-            var costFunction = new ResidualSumOfSquaresCostFunction();
-            */
+            // assume a hypothesis
+            var hypothesis = new LinearHypothesis(1);
+
+            // cost function is sum of squared errors
+            var costFunction = new ResidualSumOfSquaresCostFunction(hypothesis, trainingSet);
+
+            // define the optimization problem
+            var problem = new OptimizationProblem<double, ICostGradient<double>>(costFunction);
+
+            // define the gradient descent algorithm
+            var gd = new ResilientErrorGradientDescent();
+
+            // optimize!
+            var result = gd.Minimize(problem);
         }
     }
 }
