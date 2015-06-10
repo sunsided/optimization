201497d33abcedaa9e1c75cf973d2055518ab5bb
Markus Mayer <development@widemeadows.de>
2015-06-10 10:08:30 +0200

    started with ConjugateGradientDescent

---
+++ OptimizationTests/Hypotheses/ConjugateGradientDescent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using widemeadows.Optimization.Cost;
+using widemeadows.Optimization.GradientDescent;
+
+namespace widemeadows.Optimization.Tests.Hypotheses
+{
+    /// <summary>
+    /// Conjugate-Gradient Descent
+    /// </summary>
+    public sealed class ConjugateGradientDescent : GradientDescentBase<double, ICostGradient<double>>
+    {
+        /// <summary>
+        /// Minimizes the specified problem.
+        /// </summary>
+        /// <param name="problem">The problem.</param>
+        /// <returns>IOptimizationResult&lt;TData&gt;.</returns>
+        public override IOptimizationResult<double> Minimize(IOptimizationProblem<double, ICostGradient<double>> problem)
+        {
+
+            throw new NotImplementedException("Conjugate-Gradient Descent not implemented");
+        }
+    }
+}
