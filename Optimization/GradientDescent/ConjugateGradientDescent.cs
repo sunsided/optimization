80e0d933aa41f0f479e02b80c227ed187217f735
Markus Mayer <development@widemeadows.de>
2015-06-10 10:37:31 +0200

    last idea was a stupid idea

--- Optimization/GradientDescent/ConjugateGradientDescent.cs
+++ Optimization/GradientDescent/ConjugateGradientDescent.cs
@@ -7,14 +7,14 @@ namespace widemeadows.Optimization.Tests.Hypotheses
     /// <summary>
     /// Conjugate-Gradient Descent
     /// </summary>
-    public sealed class ConjugateGradientDescent : GradientDescentBase<double, ICostGradient2<double>>
+    public sealed class ConjugateGradientDescent : GradientDescentBase<double, ICostGradient<double>>
     {
         /// <summary>
         /// Minimizes the specified problem.
         /// </summary>
         /// <param name="problem">The problem.</param>
         /// <returns>IOptimizationResult&lt;TData&gt;.</returns>
-        public override IOptimizationResult<double> Minimize(IOptimizationProblem<double, ICostGradient2<double>> problem)
+        public override IOptimizationResult<double> Minimize(IOptimizationProblem<double, ICostGradient<double>> problem)
         {
             throw new NotImplementedException("Conjugate-Gradient Descent not implemented");
         }
