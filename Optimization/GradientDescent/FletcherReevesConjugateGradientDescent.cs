75dd075e99e741785e58c78b0bb732f6e0df9a64
Markus Mayer <development@widemeadows.de>
2015-06-12 03:23:43 +0200

    extracted a base class for secant method gradient descent algorithms

--- Optimization/GradientDescent/FletcherReevesConjugateGradientDescent.cs
+++ Optimization/GradientDescent/FletcherReevesConjugateGradientDescent.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics;
-using JetBrains.Annotations;
-using MathNet.Numerics.LinearAlgebra;
 using widemeadows.Optimization.Cost;
 
 namespace widemeadows.Optimization.GradientDescent
@@ -20,7 +18,7 @@ namespace widemeadows.Optimization.GradientDescent
     /// }
     /// </code>
     /// </remarks>
-    public sealed class FletcherReevesConjugateGradientDescent : ConjugateGradientDescentBase<double, IDifferentiableCostFunction<double>>
+    public sealed class FletcherReevesConjugateGradientDescent : SecantMethodConjugateGradientDescentBase
     {
         /// <summary>
         /// Minimizes the specified problem.
@@ -97,54 +95,6 @@ namespace widemeadows.Optimization.GradientDescent
             // that's it.
             var cost = costFunction.CalculateCost(theta);
             return new OptimizationResult<double>(cost, theta);
-        }
-
-        /// <summary>
-        /// Performs a line search by using the secant method.
-        /// </summary>
-        /// <param name="costFunction">The cost function.</param>
-        /// <param name="theta">The starting point.</param>
-        /// <param name="direction">The search direction.</param>
-        /// <returns>The best found minimum point along the <paramref name="direction"/>.</returns>
-        [NotNull]
-        private Vector<double> LineSearch([NotNull] IDifferentiableCostFunction<double> costFunction, [NotNull] Vector<double> theta, [NotNull] Vector<double> direction)
-        {
-            var maxLineSearchIteration = MaxLineSearchIterations;
-            var initialStepSize = LineSearchStepSize;
-            var epsilonSquare = ErrorToleranceSquared;
-
-            // the eta value determines how much the gradient at the current step
-            // point differs from the gradient that is orthogonal to the search direction.
-            var previousEta = costFunction.Jacobian(theta + initialStepSize*direction)*direction;
-
-            // if our step size is small enough to drop this error term under the
-            // thershold, we terminate the search.
-            var deltaD = direction*direction;
-
-            // the step size used during line search.
-            // this parameter will be adapted during search according to the change in gradient.
-            var alpha = -initialStepSize;
-
-            // iteratively find the minimum along the search direction
-            for (var j = 0; j < maxLineSearchIteration; ++j)
-            {
-                // terminate line search if alpha is close enough to zero
-                if ((alpha*alpha)*deltaD <= epsilonSquare) break;
-
-                // by multiplying the current gradient with the search direction,
-                // we'll end up with a (close to ) zero term if both directions are orthogonal.
-                // At this point, alpha will be zero (or close to it), which terminates our search.
-                var eta = costFunction.Jacobian(theta)*direction;
-
-                // adjust the step size
-                alpha = alpha*eta/(previousEta - eta);
-                previousEta = eta;
-
-                // step to the new location along the line
-                theta += alpha*direction;
-            }
-
-            return theta;
         }
     }
 }
