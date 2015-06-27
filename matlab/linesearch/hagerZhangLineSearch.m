f056abff468fd5cc9e17b3bcf4c65bd5016c2525
Markus Mayer <widemeadows@gmail.com>
2015-06-27 02:50:59 +0200

    requiring initial bracketing

--- matlab/linesearch/hagerZhangLineSearch.m
+++ matlab/linesearch/hagerZhangLineSearch.m
@@ -23,8 +23,7 @@ function [ alpha ] = hagerZhangLineSearch( x0, fun, direction )
     % fetch the initial point (required for the Wolfe conditions)
     [f0 g0] = fun(x0);
 
-    % initial values
-    alpha = 0;
+    % initial function values
     f = f0;
     g = g0;
     
@@ -36,36 +35,64 @@ function [ alpha ] = hagerZhangLineSearch( x0, fun, direction )
     delta = 0.25;
     sigma = 0.75;
     
+    % find an initial bracketing interval [a0,b0]
+    alpha = 0;
+    
+    
     % determine the next point of evaluation
     x_next = x0+alpha*direction;
-    
-    % Check for termination
 
-    % T1: Original Wolfe conditions
-    [f_next, g_next] = fun(x_next);
-    cosine      = g'*direction;
-    cosine_next = g_next'*direction;
-    if ...
-        ((f_next - f) <= (delta*alpha*cosine)) && ... % first Wolfe condition
-        (cosine_next >= sigma*cosine)                 % second Wolfe condition
-        % original Wolfe conditions are met,
-        % so alpha is our final value.
-        return;
-    end
-    
-    % T2: Approximate Wolfe conditions
-    if ...
-        ((2*delta-1)*g0 >= g_next) & ...
-        (g_next >= (sigma * g0))
-    
-        % approximate Wolfe conditions are met,
-        % so alpha may be a candidate IF there was
-        % actually a descent
-        if (f_next <= (f0 + epsilon))
+    % loop variables
+    ak = a0;
+    bk = b0;
+    k = 0;
+    while(true)
+
+        % Check for termination
+
+        % T1: Original Wolfe conditions
+        [f_next, g_next] = fun(x_next);
+        cosine      = g'*direction;
+        cosine_next = g_next'*direction;
+        if ...
+            ((f_next - f) <= (delta*alpha*cosine)) && ... % first Wolfe condition
+            (cosine_next >= sigma*cosine)                 % second Wolfe condition
+            % original Wolfe conditions are met,
+            % so alpha is our final value.
             return;
         end
-    end
 
+        % T2: Approximate Wolfe conditions
+        if ...
+            ((2*delta-1)*g0 >= g_next) & ...
+            (g_next >= (sigma * g0))
+
+            % approximate Wolfe conditions are met,
+            % so alpha may be a candidate IF there was
+            % actually a descent
+            if (f_next <= (f0 + epsilon))
+                return;
+            end
+        end
+
+        % L1: perform a double secant step
+        [a, b] = secant2(ak, bk);
+
+        % L2: select midpoint
+        if (b-a) > (gamma*(bk-ak))
+            c = (a+b)/2;
+            [a, b] = update(a, b, c);
+        end
+
+        % L3: loop
+        k = k+1;
+        ak = a;
+        bk = b;
+        alpha = c;
+
+    end
+    
+    % derp
     alpha = 1;
     
 end
