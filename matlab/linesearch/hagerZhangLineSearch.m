c79a0670e44e6df9c36cf208fa21aeab7940c10d
Markus Mayer <development@widemeadows.de>
2015-06-27 13:27:46 +0200

    added the bracket updating

--- matlab/linesearch/hagerZhangLineSearch.m
+++ matlab/linesearch/hagerZhangLineSearch.m
@@ -82,7 +82,7 @@ function [ alpha ] = hagerZhangLineSearch( x0, fun, direction )
         % L2: select midpoint
         if (b-a) > (gamma*(bk-ak))
             c = (a+b)/2;
-            [a, b] = update(a, b, c);
+            [a, b] = updateBracketing(a, b, c, fun, x0, direction, epsilon);
         end
 
         % L3: loop
@@ -95,6 +95,68 @@ function [ alpha ] = hagerZhangLineSearch( x0, fun, direction )
     
     % derp
     alpha = 1;
+    
+end
+
+function [a_bar, b_bar] = updateBracketing(a, b, c, fun, x0, direction, epsilon)
+% UPDATEBRACKETING Updates the bracketing interval.
+
+    % some values required for the tests
+    % note that they are already available from the context
+    % of the calling function, so recalculating them here is inefficient.
+    [f0, ~]  = fun(x0);
+    [fc, gc] = fun(x0 + c*direction);
+
+    % U0
+    if c<a || c>b
+        a_bar = a;
+        b_bar = b;
+        return;
+    end
+    
+    % U1
+    if gc'*direction >= 0
+        a_bar = a;
+        b_bar = c;
+        return;
+    end
+
+    % U2
+    if (gc'*direction < 0) && (fc <= f0 + epsilon)
+        a_bar = c;
+        b_bar = b;
+        return;
+    end
+    
+    % U2
+    % if (gc'*direction < 0) && (fc > f0 + epsilon)
+    a_hat = a;
+    b_hat = c;
+
+    while (true)
+
+        % U2a
+        d = (1-theta)*a_hat + theta*b_hat;
+        [fd, gd] = fun(x0 + d*direction);
+
+        if gd'*direction >= 0
+            a_bar = a_hat;
+            b_bar = d;
+            return
+        end
+
+        % U2b
+        if (gd'*direction < 0) && (fd <= f0+epsilon)
+            a_hat = d;
+            continue
+        end
+
+        % U2c
+        %if (gd'*direction < 0) && (fd > f0+epsilon)
+        b_hat = d;
+        continue;
+
+    end
     
 end
 
