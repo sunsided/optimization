8d5418bb685c622be435e42b85731a14abdd2592
Markus Mayer <widemeadows@gmail.com>
2015-07-07 17:37:37 +0200

    added interface for iteration aware line searches

---
+++ Optimization/LineSearch/IIterationAware.cs
@@ -0,0 +1,15 @@
+﻿namespace widemeadows.Optimization.LineSearch
+{
+    /// <summary>
+    /// Interface for line search algorithms that adapt behavior
+    /// in later iterations.
+    /// </summary>
+    public interface IIterationAware
+    {
+        /// <summary>
+        /// Sets the iteration.
+        /// </summary>
+        /// <param name="i">The i.</param>
+        void SetIteration(int i);
+    }
+}
